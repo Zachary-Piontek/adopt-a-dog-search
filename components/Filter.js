dbb77061e3b3c3b7046785e8f9a1accbbda1287c
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-23 12:01:46 -0700

    handle function with data

--- components/Filter.js
+++ components/Filter.js
@@ -1,17 +1,22 @@
 
 
 export default function createFilter(form, { handleFilter }) {
-    const input = form.querySelector('input');
-    const select = form.querySelector('select');
+    // const input = form.querySelector('input');
+    // const select = form.querySelector('select');
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const formData = new FormData(form);
 
         // *** call handleFilter with an object that has the breed and age;
+        handleFilter({
+            breed: formData.get('breed'),
+            age: formData.get('age'),
+        });
+        // console.log('age');
     });
 
-    return ({ breed, age }) => {
+    // return ({ breed, age }) => {
         // *** set the values of the select and input
-    };
+
 }
